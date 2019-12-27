42650ad33265c04765f83800ac7dbb646f0ed9f5
OmairSohail <halakokhan1904@gmail.com>
2019-12-27 19:18:01 +0500

    Some Work on Landing Page

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,13 @@
 <div>
 <navbar/> 
 <!--Bulma Hero Full Screen-->
- <section class="hero  is-fullheight-with-navbar">
-   <div class="hero-head">
-  
-   </div>
+ <section class="hero  is-fullheight-with-navbar firsthero">
   <div class="hero-body">
     <div class="columns">
        <div class="column is-half">
            <p class="title is-1">Manages Your Office Work</p>
            <p class="subtitle ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores iure impedit sint recusandae placeat dicta ipsum dolore! Magni quam amet distinctio obcaecati adipisci unde quod sint, illo dolorem nostrum laboriosam! Ipsa a vitae quis reprehenderit, ipsam iusto similique ut nemo, nam cupiditate vel! Ipsum iure eligendi voluptatibus non ex sit!</p>
-           <button class="button is-primary">Read More ..</button> 
+           <button class="button is-info is-rounded">Read More ..</button> 
        </div>
        <div class="column col-2">
         <!-- <template>
@@ -33,7 +30,40 @@
        </div>
     </div>
   </div>
-</section>    
+</section> 
+<!--First Hero-->
+<!--Second Hero-->
+<section class="hero is-medium">
+  <div class="hero-body">
+    <div class="container">
+      <div class="container sponsers">
+       <p class="title ">Our Sponsers</p>
+        <figure class="image is-128x128">
+          <img src="../assets/vueschool.png">
+          <img src="../assets/nativescript.png">
+           <img src="../assets/vehikl.png">
+        </figure>
+         
+      
+        
+        
+        
+      </div>
+    </div>
+  </div>
+</section>
+<!--Second Hero Ends..-->
+<!--Third Hero Starts-->
+<section class="hero is-link is-fullheight-with-navbar">
+  <div class="hero-body">
+    <div class="container">
+      <p class="title">
+        Fullheight hero with navbar
+      </p>
+    </div>
+  </div>
+</section>
+<!--Third Hero Ends-->
 </div>
 </template>
 
@@ -44,9 +74,7 @@ export default {
     navbar
   },
   methods:{
-    getImgUrl(value) {
-          return `https://picsum.photos/id/43${value}/1230/500`
-      }
+    
   }
 }
 </script>
@@ -64,5 +92,16 @@ export default {
 }
 .columns{
   margin-left:1rem;
+}
+.firsthero{
+  background-image: url('../assets/Background.jpg');
+  background-position: center;
+  background-size: cover;
+}
+.sponsers{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
